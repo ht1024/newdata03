12b55311d3a340f64b148cb3c8eb951e02b674f4
ht1024 <ht1024@users.noreply.github.com>
2020-05-05 17:29:04 -0500

    new updates

---
+++ src/components/Header/HeaderLinks copy.jsx
@@ -0,0 +1,145 @@
+/*!
+
+=========================================================
+* Material Kit React - v1.7.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/material-kit-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+/*eslint-disable*/
+import React from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
+import IconButton from "@material-ui/core/IconButton";
+// react components for routing our app without refresh
+import { Link } from "react-router-dom";
+
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Tooltip from "@material-ui/core/Tooltip";
+
+// @material-ui/icons
+import { Apps, CloudDownload } from "@material-ui/icons";
+
+// core components
+import CustomDropdown from "components/CustomDropdown/CustomDropdown.jsx";
+import Button from "components/CustomButtons/Button.jsx";
+
+import headerLinksStyle from "assets/jss/material-kit-react/components/headerLinksStyle.jsx";
+
+function HeaderLinks({ ...props }) {
+  const { classes } = props;
+  return (
+    <List className={classes.list}>
+      <ListItem className={classes.listItem}>
+        <CustomDropdown
+          noLiPadding
+          buttonText="Pages"
+          buttonProps={{
+            className: classes.navLink,
+            color: "transparent"
+          }}
+          buttonIcon={Apps}
+          dropdownList={[
+            <Link to="/" className={classes.dropdownLink}>
+              Home
+            </Link>,
+            <a
+              href="https://creativetimofficial.github.io/material-kit-react/#/documentation?ref=mkr-navbar"
+              target="_blank"
+              className={classes.dropdownLink}
+            >
+              Documentation
+            </a>,
+            <a
+              href="landing-page"
+              className={classes.dropdownLink}
+            >
+              Landing Page
+            </a>,
+            <a
+              href="new-data"
+              className={classes.dropdownLink}
+            >
+              New Data
+            </a>,
+            <a
+              href="profile-page"
+              className={classes.dropdownLink}
+            >
+              Profile Page
+            </a>,
+            <a
+              href="my-profile-page"
+              className={classes.dropdownLink}
+            >
+              My Profile Page
+            </a>
+          ]}
+        />
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          id="linkedIn-tooltip"
+          title="Find me on GitHub 1:  For IBMers"
+          placement={window.innerWidth > 959 ? "top" : "left"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            color="transparent"
+            href="https://github.ibm.com/htrista"
+            target="_blank"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + " fab fa-github"} />
+          </Button>
+        </Tooltip>
+      </ListItem><ListItem className={classes.listItem}>
+        <Tooltip
+          id="linkedIn-tooltip"
+          title="Find me on GitHub 2: Personal"
+          placement={window.innerWidth > 959 ? "top" : "left"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            color="transparent"
+            href="https://github.com/ht1024"
+            target="_blank"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + " fab fa-github"} />
+          </Button>
+        </Tooltip>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          id="linkedIn-tooltip"
+          title="Find me on LinkedIn"
+          placement={window.innerWidth > 959 ? "top" : "left"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            color="transparent"
+            href="https://www.linkedin.com/in/homerotristan/"
+            target="_blank"
+            className={classes.navLink}
+          >
+          <i className={classes.socialIcons + " fab fa-linkedin"} />
+          </Button>
+        </Tooltip>
+      </ListItem>
+    </List>
+  );
+}
+
+export default withStyles(headerLinksStyle)(HeaderLinks);
